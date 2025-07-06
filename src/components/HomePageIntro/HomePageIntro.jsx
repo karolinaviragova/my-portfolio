c457e113033a59900ffb85485084142e56b617f9
Karolina Viragova <karolinavi94@gmail.com>
2025-07-06 15:58:09 +0200

    homepahe intro component

---
+++ src/components/HomePageIntro/HomePageIntro.jsx
@@ -0,0 +1,26 @@
+import "./HomePageIntro.css"
+import { Link } from "react-router-dom";
+
+export const HomePageIntro = () => {
+  return (
+    <div className="homepage__container">
+      <h1 className="homepage__name">Karolina Viragova</h1>
+      <h2 className="homepage__role">Junior Frontend Developer</h2>
+      <div className="homepage__intro-text">
+        <p>
+          Hi, welcome to my page where I showcase my skills. I recently
+          completed a 4-month-long Digital Academy Web course by Czechitas and
+          I’m now looking for a junior frontend developer role where I can keep
+          growing and learning. Learn more{' '}
+          <Link to="/about" className="homepage__link">
+            about me
+          </Link>
+          .
+        </p>
+      </div>
+      <div className="homepage__heading">
+        <h2>My work</h2>
+      </div>
+    </div>
+  );
+};
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,8 +1,8 @@
+import './HomePage.css';
+import { HomePageIntro } from '../../components/HomePageIntro/HomePageIntro';
+
 export const HomePage = () => {
   return (
-    <div>
-      <h1>Karolina Viragova</h1>
-      <h2>Junior Frontend Developer</h2>
-    </div>
+    <HomePageIntro />
   );
 };
