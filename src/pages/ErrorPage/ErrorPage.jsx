ad4cae79f9eecdcf1e356a6a5d35c2f3a184dc77
Karolina Viragova <karolinavi94@gmail.com>
2025-07-12 17:21:17 +0200

    add error page and css

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,7 +1,11 @@
+import { Link } from 'react-router-dom';
+import './ErrorPage.css';
+
 export const ErrorPage = () => {
   return (
-    <div>
-      <h1>Tohle je error page</h1>
+    <div className="errorPage__container">
+      <h1 className="errorPage__heading">Oppps! You got lost.</h1>
+      <p className='errorPage__desc'>Try the <Link to="/" className='errorPage__link'>homepage</Link> to get back on track.</p>
     </div>
   );
 };
